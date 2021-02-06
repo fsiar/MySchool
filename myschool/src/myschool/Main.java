adde1f2ad2411ab74f0fc38ff0ab8fc4c24547ee
fatemeh siar <35432909+fsiar@users.noreply.github.com>
2021-02-07 01:40:54 +0330

    Add Code

---
+++ myschool/src/myschool/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myschool;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    
+
+
+
+        NewJFrame main = new NewJFrame()  ;
+        main.show()   ;
+        // TODO code application logic here
+    }
+
+}
